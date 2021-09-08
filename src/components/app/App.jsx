932ccf60427e2bee8835bad504c956eef98c4e12
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-08 09:49:44 -0700

    red square on screen

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
+import TileColor from '../display/TileColor';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return <TileColor color="#FF0000" />;
 }
