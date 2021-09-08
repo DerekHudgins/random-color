2fe06bb9a907924c345c8b35ece9eaaf4b5d827a
Derek Hudgins <hudgins.derek@gmail.com>
2021-09-08 09:55:42 -0700

    created colorchanger component

---
+++ src/containers/ColorChanger.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import TileColor from '../components/display/TileColor';
+
+export default class ColorCahanger extends Component {
+  render() {
+    return <TileColor color="#00FF00" />;
+  }
+}
